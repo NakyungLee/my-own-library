bdec7b99fba3c752d3fcb1deff9a33a482b09c5c
Niki Lee <ew1415048nk@gmail.com>
2017-06-25 15:49:15 +0900

    modified books page and edit page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
 
   get '/books' do
     if logged_in?
-      @user=User.find(session[:user_id])
+      @user = User.find(session[:user_id])
+      @books = Book.all
       erb :'/books/books'
     else
       flash[:message]="Please Login to access books page."
@@ -21,5 +22,15 @@ class BooksController < ApplicationController
   end
 
   post '/books/new' do
+    book = Book.create(name:params[:name],author:params[:author],user_id:session[:user_id]) if !params[:name].empty? && !params[:author].empty?
+    comment = Comment.create(comment:params[:comment],book_id:book.id)  if !params[:comment].empty?
+    sentence = Sentence.create(sentence:params[:sentence],book_id:book.id)  if !params[:sentence].empty?
+    redirect to '/books'
+  end
+
+  get '/books/edit/:book_id' do
+    @book = Book.find(params[:book_id])
+    binding.pry
+    erb :"/books/edit_books"
   end
 end
